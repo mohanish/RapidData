372d43056943085781f77896499b477ea12f45bf
mohnish86 <mohnish86@gmail.com>
2020-09-05 19:01:08 +0800

    Setup dependency injection Koin

---
+++ app/src/main/java/com/rapidata/box/DependancyModule.kt
@@ -0,0 +1,90 @@
+package com.rapidata.box
+
+import android.app.Application
+import androidx.room.Room
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.rapidata.box.database.AppDatabase
+import com.rapidata.box.database.SearchDao
+import com.rapidata.box.network.SearchApi
+import com.rapidata.ui.SearchViewModel
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val viewModelModule = module {
+    viewModel { SearchViewModel(get()) }
+}
+
+
+val apiModule = module {
+    fun provideUserApi(retrofit: Retrofit): SearchApi {
+        return retrofit.create(SearchApi::class.java)
+    }
+
+    single { provideUserApi(get()) }
+}
+
+val netModule = module {
+    fun provideCache(application: Application): Cache {
+        val cacheSize = 10 * 1024 * 1024
+        return Cache(application.cacheDir, cacheSize.toLong())
+    }
+
+    fun provideHttpClient(cache: Cache): OkHttpClient {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+            .cache(cache)
+
+        return okHttpClientBuilder.build()
+    }
+
+    fun provideGson(): Gson {
+        return GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create()
+    }
+
+
+    fun provideRetrofit(factory: Gson, client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create(factory))
+            .client(client)
+            .build()
+    }
+
+    single { provideCache(androidApplication()) }
+    single { provideHttpClient(get()) }
+    single { provideGson() }
+    single { provideRetrofit(get(), get()) }
+
+}
+
+val databaseModule = module {
+
+    fun provideDatabase(application: Application): AppDatabase {
+        return Room.databaseBuilder(application, AppDatabase::class.java, "eds.database")
+            .fallbackToDestructiveMigration()
+            .allowMainThreadQueries()
+            .build()
+    }
+
+
+    fun provideDao(database: AppDatabase): SearchDao {
+        return database.searchDao
+    }
+
+    single { provideDatabase(androidApplication()) }
+    single { provideDao(get()) }
+}
+
+val repositoryModule = module {
+    fun provideUserRepository(api: SearchApi, dao: SearchDao): BaseRepository {
+        return BaseRepository(api, dao)
+    }
+
+    single { provideUserRepository(get(), get()) }
+}
